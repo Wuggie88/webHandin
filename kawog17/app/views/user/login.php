5cc7e5959ac325f83e0b6ec88f8ea3d44e862d77
Wuggie <wuggie88@yahoo.co.uk>
2022-12-27 12:50:11 +0100

    css update and requirements update

--- kawog17/app/views/user/login.php
+++ kawog17/app/views/user/login.php
@@ -1,12 +1,12 @@
 <h1>Login</h1>
 <form method="post" action="/user/login">
-    <div>    
+    <div>
         <label for="email">email address</label>
         <br>
         <input id="email" name="email">
     </div>
 
-    <div>    
+    <div>
         <label for="password">password</label>
         <br>
         <input id="password" name="password" type="password">
